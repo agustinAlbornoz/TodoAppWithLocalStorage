d6cf670af8e817866eebf5e5db1b1054e7fcff23
agustinAlbornoz <aguswasowski@gmail.com>
2022-06-23 03:17:38 -0300

    trabajo finalizado

--- components/readTask.js
+++ components/readTask.js
@@ -1,12 +1,27 @@
 import { createTask } from "./addTask.js"
+import dateElement from "./dateElement.js";
+import { uniqueDates, orderDates } from "../services/date.js";
 
-export const readTask = () => {
+export const displayTasks = () => {
     const list = document.querySelector('[data-list]');
-    console.log(list)
+
     const taskList = JSON.parse(localStorage.getItem('task')) || [];
+    const dates = uniqueDates(taskList);
+    orderDates(dates);
 
+    dates.forEach((date) => {
+        const dateMoment = moment(date, 'DD/MM/YYYY')
 
-    taskList.forEach((task) => {
-        list.appendChild(createTask(task))
+        list.appendChild(dateElement(date));
+        taskList.forEach((task) => {
+            const taskDate = moment(task.dateFormate, 'DD/MM/YYYY')
+            const diff = dateMoment.diff(taskDate);
+            if(diff === 0){
+                list.appendChild(createTask(task));
+            };
+
+        })
     })
+
+    
 }
